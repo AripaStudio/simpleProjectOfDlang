1e763a3ff5f27d768574216cd5e404be07f0c82f
khashayar <aripastudio44@gmail.com>
2025-04-05 19:59:22 -0700

    GuessTheNumber Add update

--- GuessTheNumber/GuessTheNumber.d
+++ GuessTheNumber/GuessTheNumber.d
@@ -7,30 +7,89 @@ import std.conv;
 
 void main()
 {
-    int answerRandom = uniform(1, 50);
-    int playerGuess;
-    bool guessedCorrectly = false;
-
-    writeln("Guess the number between 1 and 50:");
-
-    while (!guessedCorrectly)
-    {
-        write("Enter your guess: ");
-        playerGuess = to!int(readln().strip());
-
-        if (playerGuess < answerRandom)
-        {
-            writeln("Too low! Try again.");
-        }
-        else if (playerGuess > answerRandom)
-        {
-            writeln("Too high! Try again.");
-        }
-        else
-        {
-            writeln("Congratulations! You guessed the correct number.");
-            readln();
-            guessedCorrectly = true;
-        }
-    }
+    writeln("Whether the game is from 1 to 50 or 1 to 100
+			For one to fifty write:
+			fifty
+			And for one to one hundred write:
+			hundred");
+	bool inputGame = false;
+	while(!inputGame)
+	{
+		string inputUser = readln().strip;
+		if(inputUser == "fifty")
+		{
+			PlayGameone_to_fifty();
+			inputGame = true;
+		}else if(inputUser == "hundred")
+		{
+			PlayGameone_to_hundred();
+			inputGame = true;
+		}else
+		{
+			writeln("Invalid input. Please enter 'fifty' or 'hundred'.");
+		}
+	}
+	
 }
+
+void PlayGameone_to_fifty()
+{
+    int answerRandom = uniform(1, 50);
+	int playerGuess;
+	bool guessedCorrectly = false;
+
+	writeln("Guess the number between 1 and 50:");
+
+	while (!guessedCorrectly)
+	{
+		write("Enter your guess: ");
+		playerGuess = to!int(readln().strip());
+
+		if (playerGuess < answerRandom)
+		{
+			writeln("Too low! Try again.");
+		}
+		else if (playerGuess > answerRandom)
+		{
+			writeln("Too high! Try again.");
+		}
+		else
+		{
+			writeln("Congratulations! You guessed the correct number.");
+			readln();
+			guessedCorrectly = true;
+		}
+	}
+}
+
+void PlayGameone_to_hundred()
+{
+	int answerRandom = uniform(1, 100);
+	int playerGuess;
+	bool guessedCorrectly = false;
+	writeln("Guess the number between 1 and 100:");
+	while (!guessedCorrectly)
+	{
+		write("Enter your guess: ");
+		playerGuess = to!int(readln().strip());
+		if (playerGuess < answerRandom)
+		{
+			writeln("Too low! Try again.");
+		}
+		else if (playerGuess > answerRandom)
+		{
+			writeln("Too high! Try again.");
+		}
+		else
+		{
+			writeln("Congratulations! You guessed the correct number.");
+			readln();
+			guessedCorrectly = true;
+		}
+	}
+
+}
+
+
+
+
