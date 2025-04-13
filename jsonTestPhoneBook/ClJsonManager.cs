8a46d436f08de311b82827dcd50697606633f9bb
khashayar <aripastudio44@gmail.com>
2025-04-13 14:10:43 -0700

    commit

--- jsonTestPhoneBook/ClJsonManager.cs
+++ jsonTestPhoneBook/ClJsonManager.cs
@@ -7,14 +7,85 @@ using System.Threading.Tasks;
 
 namespace jsonTestPhoneBook
 {
-    public class ClJsonManager
+    public class ClJsonManagerContact
     {
+
+        private const string fileName = "phoneBookCsharpJson.json";
+        private List<JsonFileCantact> contacts;
+
+        public ClJsonManagerContact()
+        {
+            loadData();
+        }
+
+        private void loadData()
+        {
+            if (File.Exists(fileName))
+            {
+                string jsonString = File.ReadAllText(fileName);
+                contacts = JsonSerializer.Deserialize<List<JsonFileCantact>>(jsonString) ?? new List<JsonFileCantact>();
+            }
+            else
+            {
+                contacts = new List<JsonFileCantact>();
+            }
+
+        }
+
+        private void saveData()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(contacts, options);
+            File.WriteAllText(fileName , jsonString);
+        }
+
+        public void AddData(string firstName, string lastName, string number)
+        {
+            var contact = new JsonFileCantact
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Number = number
+            };
+
+            contacts.Add(contact);
+            saveData();
+            Console.WriteLine("Contact added successfully!");
+        }
+
+        public void DeleteData(string firstName)
+        {
+            contacts.RemoveAll(u => u.FirstName == firstName);
+            saveData();
+            Console.WriteLine("Contact deleted successfully!");
+        }
+
+        public void ShowData()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts found!");
+                return;
+            }
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Number: {contact.Number}");
+                Console.WriteLine("---");
+            }
+        }
+
 
     }
 
-    public class JsonFile
+    public class JsonFileCantact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
+        public string Number { get; set; }
     }
 
 
--- jsonTestPhoneBook/Program.cs
+++ jsonTestPhoneBook/Program.cs
@@ -1,6 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using jsonTestPhoneBook;
 
+var manager = new ClJsonManagerContact();
+help();
+while (false)
+{
+
+}
+
+void help()
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Aripa Studio , khashayar , Dlang project");
+    Console.WriteLine("For Add , you can type : add");
+    Console.WriteLine("For Show All , you can type : show");
+    Console.WriteLine("for delete , you can type : delete ");
+    Console.WriteLine("For exit Program , you can type : exit");
+    Console.WriteLine("For Help , You can type help");
+    Console.ResetColor();
+    
+}
 
 
 
